060cd4f3103e009a7974f6a31a6dc43a730793cb
Dani Sán <40555221+Danielsjp@users.noreply.github.com>
2020-07-21 11:34:28 +0200

    scafold
    scafold

---
+++ Empleats/Data/EmpleatsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Empleats.Models;
+
+namespace Empleats.Data
+{
+    public class EmpleatsContext : DbContext
+    {
+        public EmpleatsContext (DbContextOptions<EmpleatsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Empleats.Models.Employee> Employee { get; set; }
+    }
+}
--- Empleats/Startup.cs
+++ Empleats/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Empleats.Data;
 
 namespace Empleats
 {
@@ -26,6 +28,9 @@ namespace Empleats
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<EmpleatsContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("EmpleatsContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
